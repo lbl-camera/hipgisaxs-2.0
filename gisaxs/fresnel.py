c0d5c32e73e8d67caf74376ef4f5eccc919c141c
Dinesh Kumar <dkumar@lbl.gov>
2022-08-17 14:40:09 -0700

    removed unnecessary code

--- gisaxs/fresnel.py
+++ gisaxs/fresnel.py
@@ -6,12 +6,11 @@ def basic_reflectivity(alpha, reflectivity_index):
     dns2 = 2 * reflectivity_index
     kz = xp.sin(alpha)
     kt = xp.sqrt(xp.sin(alpha)**2 - dns2)
-    Rf = (kz - kt)/(kz + kt)
-    Tf = 2 * kz / (kz + kt)
-    return  Rf, Tf
+    R = (kz - kt)/(kz + kt)
+    return  R
 
 def propagation_coeffs(alphai, alpha, reflectivity_index):
 
-    Ri, _  = basic_reflectivity(alphai, reflectivity_index)
-    Rf, _  = basic_reflectivity(alpha, reflectivity_index)
-    return [xp.single(1.), Ri, Rf, Rf * Ri ] 
+    Ri = basic_reflectivity(alphai, reflectivity_index)
+    Rf = basic_reflectivity(alpha, reflectivity_index)
+    return [xp.single(1.), Ri, Rf, Rf*Ri ] 
