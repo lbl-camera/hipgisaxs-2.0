0f645d73acdce9c98625d044586b984fe3328f3a
Dinesh Kumar <dkumar@lbl.gov>
2023-04-13 11:12:27 -0700

    changing float to double

--- src/types.h
+++ src/types.h
@@ -1,53 +1,60 @@
 
 #include <complex.h>
 
+
+#include <cuda.h>
+#include <cuda/std/complex>
+
 #ifndef HIG_TYPES__H
 #define HIG_TYPES__H
-
+#define __hstdev__ __host__ __device__
+#define __inline_hstdev__ __inline__ __host__ __device__
 
 namespace hig {
-
-    typedef std::complex<float> complex_t;
-
     struct vector3_t {
-        float x_, y_, z_;
+        double x_, y_, z_;
 
+        __hstdev__
         vector3_t(): x_(0), y_(0), z_(0) {}
 
-        vector3_t(float x, float  y, float z): x_(x), y_(y), z_(z) {}
+        __hstdev__
+        vector3_t(double x, double  y, double z): x_(x), y_(y), z_(z) {}
 
-        float norm() const {
+        __hstdev__
+        double norm() const {
             return (x_ * x_ + y_ * y_ + z_ * z_);
         }
 
-        float abs() const {
+        __hstdev__
+        double abs() const {
             return sqrt(this->norm());
         }
 
+        __hstdev__ 
         vector3_t operator+(const vector3_t &v) {
             return vector3_t(x_ + v.x_, y_ + v.y_, z_ + v.z_);
         }
 
+        __hstdev__
         vector3_t operator-(const vector3_t &v) {
             return vector3_t(x_ - v.x_, y_ - v.y_, z_ - v.z_);
         }
 
-        void operator/=(float s) {
+        __hstdev__
+        void operator/=(double s) {
             x_ /= s;
             y_ /= s;
             z_ /= s;
         }
     };
 
-    inline float dot(const vector3_t &v1, const vector3_t &v2) {
+    __inline_hstdev__
+    double dot(const vector3_t &v1, const vector3_t &v2) {
         return (v1.x_ * v2.x_ + v1.y_ * v2.y_ + v1.z_ * v2.z_);
     }
 
-    inline complex_t dot(const complex_t *q, vector3_t n) {
-        return (q[0] * n.x_ + q[1] * n.y_ + q[2] * n.z_);
-    }
-
-    inline vector3_t cross(const vector3_t &v1, const vector3_t &v2) {
+    __inline_hstdev__
+    vector3_t cross(const vector3_t &v1, const vector3_t &v2) {
         vector3_t rv;
         rv.x_ = v1.y_ * v2.z_ - v1.z_ * v2.y_;
         rv.y_ = v1.z_ * v2.x_ - v1.x_ * v2.z_;
@@ -55,18 +62,12 @@ namespace hig {
         return rv;
     }
 
+
     struct triangle_t {
         vector3_t v1;
         vector3_t v2;
         vector3_t v3;
     };
-
-
-    inline float norm3(const complex_t *a) {
-        return (a[0] * std::conj(a[0]) + 
-                a[1] * std::conj(a[1]) +
-                a[2] * std::conj(a[2])).real();
-    }
 }
 
 #endif // HIG_TYPES__H
