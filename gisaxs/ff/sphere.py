2ad040e73e7bf17e7e8b730f0b6f67b9359d0927
Dinesh Kumar <dkumar@lbl.gov>
2023-09-24 17:02:07 -0700

    simplifyign stuff

--- gisaxs/ff/sphere.py
+++ gisaxs/ff/sphere.py
@@ -1,16 +1,10 @@
 #! /usr/bin/env python
 
-from ..common import xp
-from ..common import j1
+import numpy as np
 
-def sphere(qx, qy, qz, radius, shift = None):
+def sphere(qx, qy, qz, radius):
 
-    if shift is not None:
-        qd = xp.exp(j1 * (qx * shift[0] + qy * shift[1] * qz * shift[2]))
-    else:
-        qd = 1.
-
-    vol = 4 * xp.pi * radius**3
-    qR = xp.sqrt(xp.power(qx,2) + xp.power(qy,2) + xp.power(qz,2)) * radius
-    tmp = xp.exp(-1j * qz * radius)
-    return  qd * vol * tmp *  (xp.sin(qR) - qR * xp.cos(qR)) / qR**3
+    vol = 4 * np.pi * radius**3
+    qR = np.sqrt(qx**2 + qy**2 + qz**2) * radius
+    tmp = np.exp(-1j * qz * radius)
+    return  qd * vol * tmp *  (np.sin(qR) - qR * np.cos(qR)) / qR**3
