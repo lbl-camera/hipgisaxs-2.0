d42a3dbaf368945ce5548c424c581e6cd6f6a65a
Dinesh Kumar <dkumar@lbl.gov>
2024-05-16 11:23:35 -0700

    added some more documentation

--- hipgisaxs/detector.py
+++ hipgisaxs/detector.py
@@ -21,6 +21,10 @@ class Detector:
         self.pixle_size = pixle_size
 
     @classmethod
+    """
+    Create a detector object from the JSON object.
+    """
+    
     def from_dict(cls, det):
         n = det['name'] 
         rows = det['pixel_rows']
@@ -28,6 +32,13 @@ class Detector:
         pixel = det['pixel_size']
         return cls(n, [rows, cols], pixel)        
 
+    """
+    Calculate the angles from the center of the sample to the detector.
+    
+    Parameters:
+        sdd: sample to detector distance
+        center: center of beam on the detector
+    """
     def angles(self, sdd, center):
         nrow = self.shape[0]
         ncol = self.shape[1]
@@ -43,6 +54,17 @@ class Detector:
         alpha = np.arctan2(y, tmp)
         return theta, alpha
 
+    """
+    Calculate the q-vectors for the detector.
+
+    Parameters:
+        sdd: sample to detector distance
+        center: center of beam on the detector
+        energy: energy of the x-ray
+
+    Returns:
+        qx, qy, qz: q-vectors for the detector
+    """
     def qvectors(self, sdd, center, energy):
 
         wavelen = 1.23094e+03 / energy
@@ -59,6 +81,17 @@ class Detector:
         qz = k0 * (sin(alpha))
         return qx, qy, qz
 
+    """
+    Calculate the q-values for the detector.
+
+    Parameters:
+        sdd: sample to detector distance
+        center: center of beam on the detector
+        energy: energy of the x-ray
+
+    Returns:
+        q: q-values for the detector
+    """
     def qvalues(self, sdd, center, energy):
 
         wavelen = 1230.94 / energy
@@ -66,6 +99,19 @@ class Detector:
         return np.linalg.norm(q, axis=1)
 
 
+
+    """
+    Calculate the q-vectors, including the four qz-components, for the experimental geometry.
+
+    Parameters:
+        sdd: sample to detector distance
+        center: center of beam on the detector
+        energy: energy of the x-ray
+        alphai: incident angle
+
+    Returns:
+        qx, qy, [qz1...qz4]: q-vectors for the detector
+    """
     def dwba_qvectors(self, sdd, center, energy, alphai):
 
         theta, alpha = self.angles(sdd, center)
