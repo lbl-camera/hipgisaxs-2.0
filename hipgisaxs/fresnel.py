3d241dbdb4c9c733773ae9627acd2cecf6963e0b
Dinesh Kumar <dkumar@lbl.gov>
2024-05-16 11:14:41 -0700

    Use cupy, if available. Added docstring

--- hipgisaxs/fresnel.py
+++ hipgisaxs/fresnel.py
@@ -1,6 +1,21 @@
-import numpy as np
+# Description: This file contains the function that calculates 
+# the reflection and transmission coefficients of a dielectric interface.
+
+try:
+    import cupy as np
+except ImportError:
+    import numpy as np 
 
 def propagation_coeffs(alphai, alpha, reflectivity_index):
+    """ Calculate the reflection and transmission coefficients of a dielectric interface.
+    Parameters:
+        alphai: angle of incidence
+        alpha: angle of refraction
+        reflectivity_index: refractive index of the dielectric interface
+
+    Returns:
+        [1, Ri, Rf, RiRf]: Reflection and transmission coefficients
+    """
 
     dns2 = 2 * reflectivity_index
     kz = np.sin(alphai)
